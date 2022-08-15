0837d40dd06ba26705435a317171f2862936cd97
Andreas Aronsson <BOLL@7708.net>
2022-08-15 09:40:22 +0200

    Update Newtonsoft.Json dependency & app version

--- OpenVR2WS/Properties/Resources.Designer.cs
+++ OpenVR2WS/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace OpenVR2WS.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -80,7 +80,7 @@ namespace OpenVR2WS.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to v0.40.
+        ///   Looks up a localized string similar to v1.43.
         /// </summary>
         public static string Version {
             get {
